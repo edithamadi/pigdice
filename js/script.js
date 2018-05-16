530ef6c89fc5585fd0cf15854b5f963666022f47
Edith <you@example.com>
2018-05-16 07:36:30 +0300

    modified script file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//business logic
+function rollDice1(){
+  var die1=document.getElementById("die1")
+  var die1=document.getElementById("die2")
+  var status==document.getElementById("status")
+  var d1=math.floor(math.random()*6)+1;
+  var d2=math.floor(math.random()*6)+1;
+  var diceTotal=d1+d2;
+  die1.innerHTML=d1;
+  die2.innerHTML=d1;
+  die1.innerHTML=d1;
+  status.innerHTML=diceTotal;
+  
+}
